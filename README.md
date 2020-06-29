67f8948f67d8e851286e88e6e07ee0825d4d8d75
B-Rab Jeezy <adam.c.leppek@gmail.com>
2020-06-28 18:06:27 -0600

    Complete source code and artifacts.

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # flexion-project
 Project for Flexion code exercise
+
+
+How to install & run: 
+1. Ensure that Java is installed on your machine and your Path environment 
+variable contains a path to your Java installation directory's bin folder.
+2. Access the bin folder of this repository (flexion-project), or download the 
+.jar file located there to a location of your choosing.
+3. Using the command line, navigate to the directory where the jar file is
+saved.
+4. Run the application using the following syntax:
+   java -jar <jar-filename> <inputNum> <inputUnits> <outputUnits> <studentAns>
+   a. <jar-filename> is the full filename (including .jar) of the jar you 
+      downloaded from the repository.
+   b. inputNum is the value to be converted from.
+   c. inputUnits is the unit to be converted from.
+      i. Available temperature units for conversion: 
+         Kelvin, Celsius, Fahrenheit, Rankine
+      ii. Available volume units for conversion:
+          liters, tablespoons, cubic-inches, cups, cubic-feet, gallons
+   d. outputUnits is the unit to be converted to.
+   e. studentAns is the answer provided by the student.
+   Example: java -jar ConvertUnits.jar 61.8 Fahrenheit Rankine 200.8
+5. Upon running, the command line will show whether the student's answer was
+   correct or incorrect.
+   
+   
+Future updates: 
+1. Add junit test exercising the major use cases (all the unit conversion
+   permutations, error cases, exceptions thrown, etc.)
+2. Refactor this in a simple web application: 
+   a. The GUI will contain a text box for input number and dropdowns for the 
+      input and output units. A "Calculate" button will do the calculation, and
+      the answer will be displayed to the user.
+   b. COTS solution AWS will simplify the design and usage (tools include AWS
+      Amplify and AWS Lambda).
+3. Set up a simple pipeline to automatically build, deploy, and test the app. 
+   a. AWS solution above supports running a pipeline and carrying out any tests.
+4. Update the application to allow for bulk input: 
+   a. Instead of forcing the user to type in conversion one-by-one, allow them
+      to upload a CSV file containing columns for each of the arguments.
+   b. The script or web app can then produce a table showing the result of each
+      calculation. 
+5. Inrease the available range of unit types to convert between. To accomodate 
+   a larger number of units and conversion parameters, a properties file may
+   come in handy. 
+6. Potentially beef up the app by calling existing unit conversion APIs instead
+   of redoing them all.
+   a. For example, it should be possible to call Google or Bing's conversion
+      features. 
+   b. This would also make it easier to provide a wider range of unit 
+      conversions. 
+    
+
+
+
+
+
--- src/main/ConvertUnits.java
+++ src/main/ConvertUnits.java
@@ -1,25 +1,213 @@
 package main;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+/**
+ * Class to calculate unit conversions and compare them to student answers, 
+ * returning a result of correct or incorrect. Various invalid arugment cases are 
+ * also handled. Temperature and volume units are allowed to be converted.
+ * 
+ * @author Adam Leppek
+ * @since June 2020
+ */
 public class ConvertUnits {
 
+	// Temperature Constants
+	private static final Double CELSIUS_KELVIN_OFFSET = 273.15;
+	private static final Double FAHRENHEIT_RANKINE_OFFSET = 459.67; 
+	private static final Double KELVIN_TO_RANKINE = (double)(9/5);
+		
+	// Volume Constants
+	private static final Double LITERS_TO_TBSP = 67.6280455;
+	private static final Double LITERS_TO_CU_IN = 61.0237441;
+	private static final Double LITERS_TO_CUPS = 4.22675284;
+	private static final Double LITERS_TO_CU_FT = 0.03531467;
+	private static final Double LITERS_TO_GAL = 0.26417205;
+	
 	public static void main(String[] args) {
-		// Get input: arg1 = input num; arg2 = input unit; arg3 = target unit; arg4 = student ans num
-		// Response is correct, incorrect, or invalid
-		// Correct means right answer and student answer match after rounded to tenths place
-		// Judge on input unit what to convert, temp or volume
-			// if unit is Kelvin, Celsius, Fahrenheit, Rankine, it's temp
-			// if unit is liters, tablespoons, cubic-inches, cups, cubic-feet, gallons, it's vol
-		// Do error check if the input unit isn't exactly one of these
-		// Do error check on whether the inputs are what's expected (strings or numbers)
-		// convertTemp() and convertVol() methods (these are truth): 
-			// Take input num, input unit, and target unit
-			// First convert to a standard reference unit, say Kelvin
-				// Temp: 12 possible conversions, shortened to 6 possible conversions (go both ways)
-				// Vol: 30 possible, shortened to 10 (go both ways)
-		// compare truth to student answer
-			// Get them both to tenths
-			// Provide the answer correct or incorrect
 
+		// Define the units supported by the program
+		ArrayList<String> temperatureUnits = new ArrayList<String>(Arrays.
+				asList("Kelvin", "Celsius", "Fahrenheit", "Rankine"));
+		
+		ArrayList<String> volumeUnits = new ArrayList<String>(Arrays.
+				asList("liters", "tablespoons", "cubic-inches", "cups", "cubic-feet", "gallons"));
+		
+		
+		// Get inputs and do error checking
+		Double inputNum = null;
+		String inputUnits = null;
+		String targetUnits = null;
+		Double studentAns = null;
+		
+		if (args.length != 4) {
+			throw new IllegalArgumentException("Use the correct arguments:\n" + 
+					"Usage: java ConverUnits <input number (dec.)> <input units (str.)> "
+					+ "<output units (str.)> <student answer (dec.)");
+		}
+		
+		try {
+			inputNum = Double.parseDouble(args[0]);
+		} catch (NumberFormatException e) {
+			System.err.println(e + " Invalid argument: Input number must be a decimal number.");
+		}
+		
+		if (temperatureUnits.contains(args[1]) || volumeUnits.contains(args[1])) {
+			inputUnits = args[1];
+		} else {
+			throw new IllegalArgumentException("Invalid argument: "
+					+ "Input units must be in a supported format. See README.");
+		}
+		
+		if (temperatureUnits.contains(args[2]) || volumeUnits.contains(args[1])) {
+			targetUnits = args[2];
+		} else {
+			throw new IllegalArgumentException("Invalid argument: "
+					+ "Target units must be in a supported format. See README.");
+		}
+		
+		boolean oneListContainsBoth = (temperatureUnits.contains(inputUnits) && 
+				temperatureUnits.contains(targetUnits)) || 
+				(volumeUnits.contains(inputUnits) && volumeUnits.contains(targetUnits));
+		if (!oneListContainsBoth) {
+			throw new IllegalArgumentException("Invalid arguments: "
+					+ "Input and target units must be compatible (both temperature or volume).");
+		}
+		
+		try {
+			studentAns = Double.parseDouble(args[3]);
+		} catch (NumberFormatException e) {
+			System.err.println(e + " Invalid student answer: Must be a decimal number.");
+		}
+		
+		
+		// Perform the conversion to get the right answer, depending on inputs
+		Double rightAns = null;
+		
+		if (temperatureUnits.contains(inputUnits)) {
+			rightAns = convertTemperature(inputNum, inputUnits, targetUnits);
+		} else if (volumeUnits.contains(inputUnits)) {
+			rightAns = convertVolume(inputNum, inputUnits, targetUnits);
+		}
+		
+		// Compare the right answer to the student's answer
+		String result = null;
+		
+		if (roundToTenths(studentAns).equals(roundToTenths(rightAns))) {
+			result = "correct";
+		} else {
+			result = "incorrect";
+		}
+		
+		System.out.println("Result: " + result);
 	}
-
+	
+	
+	/**
+	 * Method to convert temperature values given an input and the units to convert to/from.
+	 * 
+	 * @param inputTemp {@code <Double>} Input temp value
+	 * @param inputUnits {@code <String>} Units to convert <em>from</em>
+	 * @param targetUnits {@code <String>} Units to convert <em>to</em>
+	 * @return {@code <Double>} Converted temperature
+	 */
+	private static Double convertTemperature(Double inputTemp, String inputUnits, String targetUnits) {
+	
+		// First convert to Kelvin, a standard base unit
+		Double inputTempK = null;
+		
+		if (inputUnits.equals("Celsius")) {
+			inputTempK = inputTemp + CELSIUS_KELVIN_OFFSET;
+		} else if (inputUnits.equals("Fahrenheit")) {
+			inputTempK = (inputTemp + FAHRENHEIT_RANKINE_OFFSET)/KELVIN_TO_RANKINE;
+		} else if (inputUnits.equals("Rankine")) {
+			inputTempK = inputTemp/KELVIN_TO_RANKINE;
+		} else if (inputUnits.equals("Kelvin")){
+			inputTempK = inputTemp; 
+		}
+		
+		// Then convert Kelvin to target units
+		Double outputTemp = null;
+		
+		if (targetUnits.equals("Celsius")) {
+			outputTemp = inputTempK - CELSIUS_KELVIN_OFFSET;
+		} else if (targetUnits.equals("Fahrenheit")) {
+			outputTemp = inputTempK*KELVIN_TO_RANKINE - FAHRENHEIT_RANKINE_OFFSET;
+		} else if (targetUnits.equals("Rankine")) {
+			outputTemp = inputTempK*KELVIN_TO_RANKINE;
+		} else if (targetUnits.equals("Kelvin")) {
+			outputTemp = inputTempK;
+		}
+		
+		return outputTemp;
+	}
+	
+	
+	/**
+	 * Method to convert volume values given an input and the units to convert to/from.
+	 * 
+	 * @param inputVol {@code <Double>} Input volume value
+	 * @param inputUnits {@code <String>} Units to convert <em>from</em>
+	 * @param targetUnits {@code <String>} Units to convert <em>to</em>
+	 * @return {@code <Double>} Converted volume
+	 */
+	private static Double convertVolume(Double inputVol, String inputUnits, String targetUnits) {
+		
+		// First convert to liters, a standard base unit
+		Double inputVolL = null;
+		
+		if (inputUnits.equals("tablespoons")) {
+			inputVolL = inputVol/LITERS_TO_TBSP;
+		} else if (inputUnits.equals("cubic-inches")) {
+			inputVolL = inputVol/LITERS_TO_CU_IN;
+		} else if (inputUnits.equals("cups")) {
+			inputVolL = inputVol/LITERS_TO_CUPS;
+		} else if (inputUnits.equals("cubic-feet")) {
+			inputVolL = inputVol/LITERS_TO_CU_FT;
+		} else if (inputUnits.equals("gallons")) {
+			inputVolL = inputVol/LITERS_TO_GAL;
+		} else if (inputUnits.equals("liters")) {
+			inputVolL = inputVol;
+		}
+		
+		// Then convert liters to target units
+		Double outputVol = null;
+		
+		if (targetUnits.equals("tablespoons")) {
+			outputVol = inputVolL*LITERS_TO_TBSP;
+		} else if (targetUnits.equals("cubic-inches")) {
+			outputVol = inputVolL*LITERS_TO_CU_IN;
+		} else if (targetUnits.equals("cups")) {
+			outputVol = inputVolL*LITERS_TO_CUPS;
+		} else if (targetUnits.equals("cubic-feet")) {
+			outputVol = inputVolL*LITERS_TO_CU_FT;
+		} else if (targetUnits.equals("gallons")) {
+			outputVol = inputVolL*LITERS_TO_GAL;
+		} else if (targetUnits.equals("liters")) {
+			outputVol = inputVolL;
+		}
+		
+		return outputVol;
+	}
+	
+	
+	/**
+	 * Method to round a double number to the tenths place.
+	 * 
+	 * @param ans {@code <Double>} Number to be rounded
+	 * @return {@code <Double>} Rounded number
+	 */
+	private static Double roundToTenths(Double ans) {
+		
+		// a. Multiply ans by 10 to get the tenths place in the ones place
+		// b. Round this to a long (no decimal)
+		// c. Convert it back to a double and divide by 10, putting the new (rounded)
+		//    ones place back into the old tenths place
+		Double ansTenths = Long.valueOf(Math.round(ans*10)).doubleValue()/10;
+		
+		return ansTenths;
+	}
 }
